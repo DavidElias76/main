ac95c2670370907b39e63a7fd5d97a64b1e720e6
DavidElias76 <eliaskimutai76@gmail.com>
2024-10-21 12:03:05 +0300

    created new files and commiting them to the repository

---
+++ Object Oriented Technologies/example 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float number1, number2, sum, average; //declaration of variables
+    cout<<"Enter two numbers";
+    cin >>number1;
+    cin >>number2;
+
+    sum = number1+number2;
+    average= sum/2;
+
+    
+
+
+
+
+}
---
+++ Object Oriented Technologies/example.cpp
@@ -0,0 +1,8 @@
+#include<iostream> // this is a haeder file
+
+using namespace std;
+int main(){
+    cout << "C++ is better than c\n";
+
+    return 0;
+}
