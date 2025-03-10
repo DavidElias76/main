0108c3b77bde7d3b6f1949b7cfd6d980845a4eb2
DavidElias76 <eliaskimutai76@gmail.com>
2025-03-11 00:31:14 +0300

    committing all files to javascript folder and created a new sims project folder

--- Javascript Folder/testing.js
+++ Javascript Folder/testing.js
@@ -1,14 +1,31 @@
-const saviour = {
-    firstName: "Mary",
-    lastName: "Ann",
-    age: 30,
-    eyeColor: "green"
-};
+// const saviour = {
+//     firstName: "Mary",
+//     lastName: "Ann",
+//     age: 30,
+//     eyeColor: "green"
+// };
 
-// adding a new method
+// // adding a new method
 
-saviour.fullname = function(){
-    return this.firstName + " " + this.lastName;
-};
+// saviour.fullname = function(){
+//     return this.firstName + " " + this.lastName;
+// };
 
-console.log(saviour.fullname());
+// console.log(saviour.fullname());
+
+// 
+
+
+
+//  arrays practice
+
+// var mylist =["appples", "oranges", "bananas"];
+
+// mylist.forEach(function(value, index) {
+//     console.log(value, index);
+// });
+
+
+
+// 
+
